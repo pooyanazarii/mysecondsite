c8d212c1059896e3b3f79c58302c5f9e9d4e3725
Pooya Nazari <66327968+pooyanazarii@users.noreply.github.com>
2022-08-14 22:39:10 +0430

    add a page

--- mysecondsite/urls.py
+++ mysecondsite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mysecondsite.views import mydif
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('mymy/',mydif)
 ]
---
+++ mysecondsite/views.py
@@ -0,0 +1,3 @@
+from django.http import HttpResponse
+def mydif(r):
+    return HttpResponse("This is a text on view.py file")
