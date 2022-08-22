f5202369a5c4e27d391d27ba09a50b4855070e76
Pooya Nazari <66327968+pooyanazarii@users.noreply.github.com>
2022-08-22 20:25:03 +0430

    edit html files and url address

--- myblog/views.py
+++ myblog/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 contex_index = {"titel":"This is dictionary from view.py"}
 def view_index(request):
     return render(request,"index.html",contex_index)
+
 contex_about = {"titel":"This about view"}
 def view_about(request):
+    
     return render(request,"about.html",contex_about)
