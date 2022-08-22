d264fcc91c36b559829ae6e6613d9de19f95ad6a
Pooya Nazari <66327968+pooyanazarii@users.noreply.github.com>
2022-08-22 20:52:41 +0430

    added models and registered table in admin

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from myblog.models import Post
 # Register your models here.
+@admin.register(Post)
+class Post_Admin (admin.ModelAdmin):
+    empty_value_display = '-empty-'
+    #list_display = ['title']
+    search_fields = ['content','title']
